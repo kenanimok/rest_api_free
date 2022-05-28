183eed95a1ac027242701a8e73543c5e1eb64d23
kenanimok <89970690+kenanimok@users.noreply.github.com>
2022-05-28 17:57:45 +0700

    create Users

---
+++ src/UserCreate.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { Typography,Grid, Button } from '@mui/material';
+import TextField from '@mui/material/TextField';
+
+
+
+export default function UsersCreate() {
+   const [fname,setFname]=useState("");
+   const [lname,setLname]=useState("");
+   const [username,setUsername]=useState("");
+   const [email,setEmail]=useState("");
+   const [avatar,setAvatar]=useState("");
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm" sx={{pa:2}}>
+          <Typography variant="h6" gutterBottom component="div">
+               Create Users
+            </Typography>
+            <form>
+               <Grid container spacing={2}>
+                 <Grid item xs={12} > 
+                   <TextField id="fname" label="Firstname" variant="outlined" fullWidth required/>
+                 </Grid>
+                 <Grid item xs={12} sm={6}> 
+                   <TextField id="lname" label="lastname" variant="outlined" fullWidth required/>
+                 </Grid>
+                 <Grid item xs={12} sm={6}> 
+                   <TextField id="username" label="username" variant="outlined" fullWidth required/>
+                 </Grid>
+                 <Grid item xs={12} sm={6}> 
+                   <TextField id="email" label="Email" variant="outlined" fullWidth required/>
+                 </Grid>
+                 <Grid item xs={12}> 
+                   <TextField id="avatar" label="avatar" variant="outlined" fullWidth required/>
+                 </Grid>
+                 <Grid item xs={12}> 
+                  <Button variant="contained" fullWidth>Create</Button>
+                 </Grid>
+               </Grid>
+            </form>
+      </Container>
+    </React.Fragment>
+  );
+}
---
+++ src/Users.js
@@ -0,0 +1,105 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useState ,useEffect} from "react";
+import { FiveG } from "@mui/icons-material";
+import Avatar from '@mui/material/Avatar';
+import Link from '@mui/material/Link';
+
+
+
+
+
+
+export default function Users() {
+
+      
+   const [items,setItems] = useState([]);
+//    const [isLoaded,setIsLoaded]= useState(false);
+//    const [error,setError]=useState(null)
+
+   useEffect(()=>{
+        fetch("https://www.mecallapi.com/api/users")
+        .then(res=> res.json())
+        .then(
+            (result)=>{
+                // setIsLoaded(true);
+                setItems(result);
+        
+            }
+            ,
+            // (error)=>{
+            //     setIsLoaded(true);
+            //     setError(error);
+            // }
+        )
+   },[])
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg" sx={{ p: 2 }}>
+        <Paper elevation={3} />
+        <Box display="flex">
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" gutterBottom component="div">
+              {" "}
+              Users
+            </Typography>
+          </Box>
+          <Box>
+            <Link href="create">
+                <Button variant="contained">Users</Button>
+            </Link>
+          </Box>
+        </Box>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="right"></TableCell>
+                <TableCell align="right">Fist Name</TableCell>
+                <TableCell align="right">Last Name</TableCell>
+                <TableCell align="right">Username</TableCell>
+                <TableCell align="right">Action</TableCell>
+
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {items.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell align="right"><Avatar alt="Remy Sharp" src={row.avatar} /></TableCell>
+                  <TableCell align="right">{row.fname}</TableCell>
+                  <TableCell align="right">{row.lname}</TableCell>
+                  <TableCell align="right">{row.username}</TableCell>
+                  {/* <TableCell align="right">{row}</TableCell> */}
+
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <Paper />
+      </Container>
+    </React.Fragment>
+  );
+}
